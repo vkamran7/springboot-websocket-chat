ccc3ce8f33dadd06f49dc67119cbaa9844997896
kamranvalizada <vkamran7@gmail.com>
2021-11-11 16:58:23 +0400

    adding chat model

---
+++ src/main/java/com/chat/websocket/model/ChatMessage.java
@@ -0,0 +1,35 @@
+package com.chat.websocket.model;
+
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType messageType;
+
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+}
